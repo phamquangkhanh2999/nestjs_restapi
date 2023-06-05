29bd276683cd6e2e29b729da7b76e7ab6a523dd9
QuangKhanh <58257872+phamquangkhanh2999@users.noreply.github.com>
2023-06-05 16:18:44 +0700

    update login register

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+  async register(authDto: AuthDto) {
+    try {
+      const hashedPassword = await argon.hash(authDto.password);
+      const user = await this.prismaService.user.create({
+        data: {
+          email: authDto.email,
+          hashedPassword: hashedPassword,
+          firstName: '',
+          lastName: '',
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Error in credentials');
+      }
+    }
+  }
+  async login(authDto: AuthDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: authDto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('user not found');
+    }
+    const passwordMatched = await argon.verify(
+      user.hashedPassword,
+      authDto.password,
+    );
+    if (!passwordMatched) {
+      throw new ForbiddenException('Incorrect password');
+    }
+    return user;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 /**
  * How to generate a module
  * nest g module "module"
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
